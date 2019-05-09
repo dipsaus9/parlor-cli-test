0555422b4ce7eda9185057c62f87616f5abd41b0
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-05-09 13:04:29 +0200

    init

---
+++ parlor.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  projectId: 5,
+  username: "dipsaus",
+  password: "admin",
+  typoSettingsFilename: "_usage.scss",
+  typoEmbedFilename: "_embed.scss",
+  colorFilename: "_custom-colors.scss",
+  grid: "grid",
+  gridFilename: "_grid.scss",
+  fonts: "scss",
+  images: "static",
+  colors: "color",
+  typoFolder: "typography"
+};
