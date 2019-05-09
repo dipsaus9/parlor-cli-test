c040f29b550dbca46483d9296a09eb7d45fe0f9f
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-05-09 13:10:36 +0200

    empty project for testing

---
+++ layouts/default.vue
@@ -0,0 +1,5 @@
+<template>
+	<div class="layout layout-default">
+		<nuxt />
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<main class="page page--home">
+		<section class="intro">
+			<h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h1>
+			<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
+			<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
+			<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
+			<h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h5>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
+		</section>
+	</main>
+</template>
+
+<style lang="scss">
+@import '~tools';
+
+.page {
+	// general page styles
+	&--home {
+		// specific home styles
+	}
+}
+
+.intro {
+	height: 100vh;
+	width: 100vw;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	flex-direction: column;
+}
+</style>
