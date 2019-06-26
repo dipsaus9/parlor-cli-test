6ccede8c2a76f5454d11d99bbeebeb7ae8ae509b
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-06-26 17:29:34 +0200

    Ready for tommorow

--- parlor.config.js
+++ parlor.config.js
@@ -1,16 +1,16 @@
 const env = require('./config/latest');
 
 module.exports = {
-	projectId: 7,
+	projectId: 20,
 	username: env.username,
-	password: env.password
-	// typoSettingsFilename: '_usage.scss',
-	// typoEmbedFilename: '_embed.scss',
-	// colorFilename: '_custom-colors.scss',
-	// gridFilename: '_grid.scss',
-	// fonts: 'assets/fonts',
-	// images: 'static/images',
-	// colors: 'assets/scss/color',
-	// grid: 'assets/scss/grid',
-	// typoFolder: 'assets/scss/typography'
+	password: env.password,
+	typoSettingsFilename: '_usage.scss',
+	typoEmbedFilename: '_embed.scss',
+	colorFilename: '_custom-colors.scss',
+	gridFilename: '_grid.scss',
+	fonts: 'assets/fonts',
+	images: 'static/images',
+	colors: 'assets/scss/color',
+	grid: 'assets/scss/grid',
+	typoFolder: 'assets/scss/typography'
 };
