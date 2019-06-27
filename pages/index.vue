eaecc7796b1ebd02b84192c117ea12f205639e76
Dennis Dipsaus <dennis.spierenburg@hotmail.com>
2019-06-27 10:54:13 +0200

    no headers lefT

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,6 @@
 			<h2>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h2>
 			<h3>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h3>
 			<h4>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h4>
-			<h5>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h5>
-			<h6>Lorem ipsum dolor sit amet, consectetur adipisicing elit</h6>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit</p>
 		</section>
 	</main>
